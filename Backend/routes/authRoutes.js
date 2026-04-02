e16c79e21ac8105a29a0e6e63620b51d42dfce40
J.D.Jayatilake <84485753+dillonjayatilake@users.noreply.github.com>
2026-04-02 17:14:58 +0530

    Update authRoutes.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { login, signup, seedUsers } = require('../controllers/authController');
+const { login, signup, seedUsers, resetDatabase } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/signup', signup);
 router.post('/seed-users', seedUsers);
+router.post('/reset-db', resetDatabase);
 
 module.exports = router;
